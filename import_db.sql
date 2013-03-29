90246d70517d12eebf0947ce546033d8245d0862
India Meisner <india.rae@gmail.com>
2013-03-29 10:13:25 -0700

    Add data to tables and fix syntax issues

--- import_db.sql
+++ import_db.sql
@@ -6,13 +6,29 @@ CREATE TABLE chef(
 );
 
 
-INSERT INTO chef (id, fname, lname, mentor)
+INSERT INTO chef (id, first_name, last_name, mentor)
 VALUES (NULL, 'Julia', 'Child', NULL),
        (NULL, 'Gordon', 'Ramsey', 1),
        (NULL, 'Homaru', 'Cantu', 2),
-       (NULL 'Cookie', 'McFancypants', 2)
+       (NULL, 'Cookie', 'McFancypants', 2),
        (NULL, 'Adam', 'Zeisler', 3);
 
+
+CREATE TABLE restaurant(
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  chef_id INTEGER,
+  neighborhood VARCHAR(255),
+  cuisine VARCHAR(255)
+);
+
+INSERT INTO restaurant (id, name, neighborhood, cuisine)
+VALUES (NULL, 'PBS Studio', 'PBS', 'French'),
+       (NULL, 'Naglee Park Garage', 'Naglee Park', 'American'),
+       (NULL, 'Moto', 'Downtown', 'Molecular Gastronomy'),
+	   (NULL, 'Otom', 'Downtown', 'Molecular Gastronomy'),
+       (NULL, 'BurgerJoint', 'Downtown', 'American'),
+	   (NULL, 'FoodNetwork', 'TV Land', 'British');
 
 CREATE TABLE cheftenure(
   id INTEGER PRIMARY KEY,
@@ -23,18 +39,24 @@ CREATE TABLE cheftenure(
   is_head_chef INTEGER
 );
 
-CREATE TABLE restaurant(
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,,
-  chef_id INTEGER,
-  neighborhood VARCHAR(255),
-  cuisine VARCHAR(255)
-);
+INSERT INTO cheftenure (id, chef_id, restaurant_id, start_date, end_date, is_head_chef)
+VALUES (NULL, 1, 1, '1956-12-12', '1990-06-21', 1),
+       (NULL, 4, 5, '2001-07-14', '2009-02-28', 0),
+       (NULL, 4, 2, '2009-03-21', '2013-03-28', 1),
+       (NULL, 2, 6, '2000-01-01', '2012-09-17', 1),
+       (NULL, 3, 3, '2004-02-11', '2009-09-11', 1),
+	   (NULL, 5, 3, '2007-03-08', '2009-09-11', 0);
 
 CREATE TABLE critic(
   id INTEGER PRIMARY KEY,
   screen_name VARCHAR(50) NOT NULL
 );
+
+INSERT INTO critic (id, screen_name)
+VALUES (NULL, 'food_judge'),
+       (NULL, 'live_to_eat'),
+       (NULL, 'feed_mee'),
+       (NULL, 'yumm_yumm');
 
 CREATE TABLE restaurantreview(
   id INTEGER PRIMARY KEY,
@@ -44,3 +66,11 @@ CREATE TABLE restaurantreview(
   review_date TEXT,
   review TEXT 
 );
+
+INSERT INTO restaurantreview (id, restaurant_id, critic_id, score, review_date, review)
+VALUES (NULL, 3, 1, 15, '2005-09-15', 'Slow service, but pretty good overall'),
+       (NULL, 3, 4, 19, '2009-07-24', 'Awesome sauce!'),
+       (NULL, 5, 1, 3, '2012-10-21', 'This place sucks'),
+       (NULL, 6, 3, 13, '2008-08-08', 'Chef here is scary and loud!'),
+       (NULL, 2, 2, 17, '2010-04-21', 'Guy Fieri ate here!'),
+	   (NULL, 2, 1, 14, '2013-01-16', 'Too expensive');
